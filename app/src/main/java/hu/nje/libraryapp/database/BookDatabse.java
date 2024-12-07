e9c39f53a470a2f5f96b440b727dfc933301675b
BotondBalassa <baboti20@gmail.com>
2024-12-07 21:23:18 +0100

    Update BookDatabse.java

--- app/src/main/java/hu/nje/libraryapp/database/BookDatabse.java
+++ app/src/main/java/hu/nje/libraryapp/database/BookDatabse.java
@@ -1,4 +1,30 @@
 package hu.nje.libraryapp.database;
 
-public class BookDatabse {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import hu.nje.libraryapp.BookDao;
+import hu.nje.libraryapp.model.Book;
+
+@Database(entities = {Book.class}, version = 1, exportSchema = false)
+public abstract class BookDatabase extends RoomDatabase {
+
+    private static BookDatabase instance;
+
+    // DAO interfész
+    public abstract BookDao bookDao();
+
+    // Singleton minta
+    public static synchronized BookDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                            BookDatabase.class, "book_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
 }
